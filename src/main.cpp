bc94d124b0a38471fd1bc4da6e054f9d39de8819
ysmaelsa464@gmail.com <ysmaelsa464@gmail.com>
2026-04-24 19:18:53 -0300

    Primeiro commit - projeto ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PHOTORESISTOR 4
+#define LED 16
+#define BUTTON 23
+float percentage = 0.0;
+LiquidCrystal_I2C lcd(0x27, 21, 22); 
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(PHOTORESISTOR, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  lcd.init(); 
+  lcd.backlight(); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  int sensorValue = analogRead(PHOTORESISTOR);
+  percentage = 100 - (sensorValue / 4095.0) * 100.0;
+  if(sensorValue < 2866) {
+    digitalWrite(LED, LOW); 
+  } else {
+    digitalWrite(LED, HIGH); 
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if(digitalRead(BUTTON) == HIGH) {
+    lcd.setCursor(0, 0);
+    lcd.print("Valor Bruto:");
+    lcd.print(analogRead(PHOTORESISTOR));
+    lcd.print("%");
+
+    lcd.setCursor(0, 1);
+    lcd.print("LED: ");
+    lcd.print(digitalRead(LED) == HIGH ? "HIGH " : "LOW");
+  }else{
+    lcd.setCursor(0, 0);
+    lcd.print("Nivel de Luz:");
+    lcd.print(percentage, 0);
+    lcd.print("%");
+
+    lcd.setCursor(0, 1);
+    lcd.print("LED: ");
+    lcd.print(digitalRead(LED) == HIGH ? "ON " : "OFF");
+  }
+
+  
 }
